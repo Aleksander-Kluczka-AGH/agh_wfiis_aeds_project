8334d1236c5e60b0545976fc7624a874ab5156b6
vis4rd <alagaesia.vrael@gmail.com>
2024-01-25 06:13:08 +0100

    Add notifications to information service

--- information-service/build.gradle.kts
+++ information-service/build.gradle.kts
@@ -13,31 +13,15 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
     testImplementation("org.mockito:mockito-junit-jupiter:3.12.4")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.mockk:mockk:1.12.0")
-
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-
-//    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-//    implementation("io.micrometer:micrometer-registry-prometheus")
-//    implementation("net.logstash.logback:logstash-logback-encoder")
-
-//    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-//    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-
-//    implementation("com.auth0:java-jwt")
-
-//    testImplementation("org.junit.jupiter:junit-jupiter-api")
-//    testImplementation("io.strikt:strikt-core")
-//    testImplementation("io.mockk:mockk")
-
-//    integTestImplementation("org.springframework.boot:spring-boot-starter-test")
-//    integTestImplementation("org.springframework.security:spring-security-test")
-//    integTestImplementation("org.testcontainers:mongodb")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
